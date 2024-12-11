10155da746d2994ec496ce0e83cd8e4cbfc7e655
Martín Bravo <123977407+MartinEBravo@users.noreply.github.com>
2024-12-11 22:27:08 +0100

    add: RL3

--- RL3/student_3_2_1.py
+++ RL3/student_3_2_1.py
@@ -3,19 +3,19 @@
 rewards = [10, -10, -10, 10, -10, 10, -10, 10, -10, 10, -10, 10, -10]
 
 # Q learning learning rate
-alpha = 0
+alpha = 0.1
 
 # Q learning discount rate
-gamma = 0
+gamma = 0.9
 
 # Epsilon initial
 epsilon_initial = 1
 
 # Epsilon final
-epsilon_final = 1
+epsilon_final = 0.1
 
 # Annealing timesteps
-annealing_timesteps = 1
+annealing_timesteps = 100
 
 # threshold
 threshold = 1e-6
